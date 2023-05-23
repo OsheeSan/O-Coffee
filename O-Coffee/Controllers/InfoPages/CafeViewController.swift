e9d4be8b82769f649008d564c7745a8f24a64d57
Anton Babko <anton.babko@ukma.edu.ua>
2023-05-23 15:43:09 +0300

    Cafe view images

---
+++ O-Coffee/Controllers/InfoPages/CafeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CafeViewController.swift
+//  O-Coffee
+//
+//  Created by admin on 22.05.2023.
+//
+
+import UIKit
+
+class CafeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ O-Coffee/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  O-Coffee
+//
+//  Created by admin on 23.05.2023.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
